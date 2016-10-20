871539adaa9e10ab6006c62d86213cc7f4414736
maximedelpit <maximedelpit@gmail.com>
2016-10-20 10:32:17 +0200

    adding url on top of conf-call modal

--- app/assets/javascripts/conf_call.js
+++ app/assets/javascripts/conf_call.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   $('.launch-conf').click(function(event) {
+    $('.url-conf-title').remove();
     var room_url = "https://appear.in/" + $(this).data('room-name')
     conf_iframe = '<iframe src=' + room_url + 'class="responsive-img materialboxed" width="100%" height="600px" frameborder="0"></iframe>'
     $('#pending-call .conf-part').html(conf_iframe);
-    $('#pending-call').openModal()
+    $('#pending-call').openModal();
+    $('#pending-call .modal-content').prepend('<h3 class="url-conf-title">Please share url with attendees : '+room_url+'</h3>');
   });
 });
